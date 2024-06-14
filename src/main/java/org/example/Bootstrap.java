a954f33ca33c0cd40af7242a2d031e3d28b38d52
apocarteres <apocarteres@gmai.com>
2024-06-14 16:38:11 +0300

    improved reporting output. Now we print all periods, not just only M1

--- src/main/java/org/example/Bootstrap.java
+++ src/main/java/org/example/Bootstrap.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.util.Random;
 import java.util.concurrent.Executors;
@@ -8,23 +9,39 @@ import static java.lang.Thread.sleep;
 import static org.example.Period.M1;
 
 public class Bootstrap {
+
+  public static final int MINUTES_DELAY_REPORTING = 1;
+
   public static void main(String[] args) {
     var service = new InMemoryTrendBarService();
-    try (var pool = Executors.newFixedThreadPool(4)) {
-      // the background job which build history upon received quotes
+    try (var pool = Executors.newFixedThreadPool(5)) {
+      // 2 background jobs, which build history upon received quotes
       pool.submit(() -> {
         while (true) {
           service.updateHistory();
           sleep(100);
         }
       });
+      pool.submit(() -> {
+        while (true) {
+          service.updateHistory();
+          sleep(150);
+        }
+      });
 
-      //some job, which feeds the service with quotes
+      //2 jobs, which feeds the service with quotes
       pool.submit(() -> {
         while (true) {
           var r = new Random().ints(250, 800);
           service.feed(Symbol.EURJPY, r.findAny().orElse(0));
-          sleep(10);
+          sleep(50);
+        }
+      });
+      pool.submit(() -> {
+        while (true) {
+          var r = new Random().ints(900, 999);
+          service.feed(Symbol.EURJPY, r.findAny().orElse(0));
+          sleep(50);
         }
       });
 
@@ -32,12 +49,17 @@ public class Bootstrap {
       pool.submit(() -> {
         var now = Instant.now().getEpochSecond();
         while (true) {
-          var history = service.history(Symbol.EURJPY, M1, now);
-          for (Bar bar : history) {
-            System.out.println(bar);
+          System.out.printf("(!) reports generated once per %d minute%n", MINUTES_DELAY_REPORTING);
+          for (var period : Period.values()) {
+            System.out.println(period);
+            var history = service.history(Symbol.EURJPY, period, now);
+            for (Bar bar : history) {
+              System.out.print(" - ");
+              System.out.println(bar);
+            }
           }
           System.out.println("---");
-          sleep(1000);
+          sleep(Duration.ofMinutes(MINUTES_DELAY_REPORTING).toMillis());
         }
       });
     }
